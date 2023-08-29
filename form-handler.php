0fb0b65672b60e7e311533017f52dcce716c1697
Acemacman <139581514+Acemacman@users.noreply.github.com>
2023-08-29 10:46:46 +0100

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'info@equinecremationservices.co.uk';
+
+$email_subject = 'Messsage from Cremation Site';
+
+$email_body = "UserName: $name.\n".
+                "User Email: $visitor_email.\n".
+                    "Message: $message.\n";
+
+$to = 'andrew@apgoatman.co.uk';
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+?>
